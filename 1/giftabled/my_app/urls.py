9e7b9cebea33a5daaf77c87b05d7853c60f71009
Jagannath R Kulakarni <jagannathrkulakarni.171845@gmail.com>
2022-05-29 21:05:18 +0530

    login system

--- 1/giftabled/my_app/urls.py
+++ 1/giftabled/my_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.home_page,name='home_page'),
-    path('login',views.login_page,name='login_page')
+    path('login',views.login_page,name='login_page'),
+    path('signup',views.signup_page,name='signup_page'),
+      path('logout',views.log_out,name='logout_page')
 ]
--- 1/giftabled/my_app/views.py
+++ 1/giftabled/my_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 
@@ -12,4 +15,46 @@ def home_page(request):
 
 
 def login_page(request):
+    
+    if(request.method == 'POST'):
+        username=request.POST['username']
+        password=request.POST['password']
+        
+        user=authenticate(username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            msg='Hello '+username+', You are successfully logged in'
+            messages.success(request,msg)
+            return redirect('/')
+        else:
+            messages.info(request,"Credentials Invalid")    
+            return redirect('/login')
+    
     return render(request,'my_app/login.html')
+
+def signup_page(request):
+    
+    if(request.method == 'POST'):
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        
+        myuser=User.objects.create_user(username,email,password)
+        myuser.save()
+        messages.success(request,"Your Account has been successfully created")
+        
+        return redirect('/login')
+    
+    if request.user.is_authenticated:
+        return redirect('/') 
+       
+    return render(request,'my_app/signup.html')
+
+
+def log_out(request):
+    logout(request)
+    messages.success(request,"Succesfully logged out")
+    
+    return redirect('/login')
+    
